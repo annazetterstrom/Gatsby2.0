50155967e4294ad8d6b4dbf5e7b6a0087b84a635
annazetterstrom <anna.wiktoria@me.com>
2019-08-29 18:52:25 +0200

    commit

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,16 +4,11 @@ import Layout from "../components/layout"
 
 export default () => (
   <Layout>
-  <div style={{ color: `purple` }}>
-    <Header headerText="My projects" />
-    <p>Below you can see what projects I´ve been working on. </p>
+  <div style={{ color: `teal` }}>
+    <Header headerText="Contact" />
+    <p>Shoot me an email <a href="mailto:anna.wiktoria@me.com">here</a></p>
     <img src="https://source.unsplash.com/random/400x200" alt="" />
   </div>
 
-  <canvas id="pie-chart" width="300" height="auto"></canvas>
-
-  
   </Layout>
-  
 )
-
